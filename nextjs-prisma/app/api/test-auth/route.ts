e8f49fdf1e4d0e53d152acb67c5c236e5785fa3f
Priyanshu Sinha <PriyanshuSinha636@gmail.com>
2025-12-25 19:32:05 +0530

    Authentication-backend

---
+++ nextjs-prisma/app/api/test-auth/route.ts
@@ -0,0 +1,19 @@
+import { NextRequest , NextResponse } from "next/server";
+import { requireAuth } from "@/lib/auth-helpers";
+
+export async function GET(req : NextRequest){
+    const user = await requireAuth()
+
+    if(user instanceof NextResponse){
+        return user
+    }
+
+    return NextResponse.json({
+        message: "You are authenticated",
+        user :{
+            id : user.id,
+            email : user.email,
+            name : user.name,
+        }
+    })
+}
---
+++ nextjs-prisma/lib/auth-helpers.ts
@@ -0,0 +1,20 @@
+import { getServerSession } from "next-auth";
+import { authOptions } from "./auth";
+import { NextResponse } from "next/server";
+
+export async function getCurrentUSer(){
+    const session = await getServerSession(authOptions)
+    return session?.user
+}
+
+export async function requireAuth(){
+    const user = await getCurrentUSer()
+
+    if(!user){
+        return NextResponse.json({
+            error: "UnAauthorized User!!"
+        },
+    {status : 401})
+    }
+    return user;
+}
